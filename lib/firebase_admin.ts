e24912796f5fc40c450b4e6772039f7bebd6a009
Bishal Mondal <bishal.mondal@badibahen.in>
2025-11-17 14:19:58 +0530

    fix: fixing ui and form

--- lib/firebase_admin.ts
+++ lib/firebase_admin.ts
@@ -6,8 +6,13 @@ import { firebase_database_url } from "@/constants/firebase";
 let app: admin.app.App;
 
 if (!admin.apps.length) {
+  const finalServiceAccount =
+    serviceAccount ||
+    (JSON.parse(
+      process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}",
+    ) as ServiceAccount);
   app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    credential: admin.credential.cert(finalServiceAccount as ServiceAccount),
     databaseURL: firebase_database_url,
   });
 } else {
