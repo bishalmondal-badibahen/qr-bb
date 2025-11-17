ee4d683673f0821516e88ced7551a29502afba64
Bishal Mondal <bishal.mondal@badibahen.in>
2025-11-17 14:47:57 +0530

    fix: fixing ui and form

--- constants/firebase.ts
+++ constants/firebase.ts
@@ -1,3 +1,5 @@
+import { ServiceAccount } from "firebase-admin";
+
 export const firebase_api_key = process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "";
 export const firebase_auth_domain =
   process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "";
@@ -19,4 +21,34 @@ export const firebase_config = {
   storageBucket: firebase_storage_bucket,
   messagingSenderId: firebase_messaging_sender_id,
   appId: firebase_app_id,
+};
+
+export const firebase_service_type = process.env.FIREBASE_ACCOUNT_KEY || "";
+export const firebase_service_email = process.env.FIREBASE_CLIENT_EMAIL || "";
+export const firebase_service_id = process.env.FIREBASE_CLIENT_ID || "";
+export const firebase_service_uri = process.env.FIREBASE_AUTH_URI || "";
+export const firebase_service_token_uri = process.env.FIREBASE_TOKEN_URI || "";
+export const firebase_service_cert_url = process.env.FIREBASE_CERT_URL || "";
+export const firebase_service_client_cert_url =
+  process.env.FIREBASE_CLIENT_CERT_URL || "";
+export const firebase_service_universe_domain =
+  process.env.FIREBASE_UNIVERSE_DOMAIN || "";
+
+export const firebase_service_private_key_id =
+  process.env.FIREBASE_PRIVATE_KEY_ID || "";
+export const firebase_service_private_key =
+  process.env.FIREBASE_PRIVATE_KEY || "";
+
+export const serviceAccount = {
+  type: firebase_service_type,
+  project_id: firebase_project_id,
+  private_key_id: firebase_service_private_key_id,
+  private_key: firebase_service_private_key,
+  client_email: firebase_service_email,
+  client_id: firebase_service_id,
+  auth_uri: firebase_service_uri,
+  token_uri: firebase_service_token_uri,
+  auth_provider_x509_cert_url: firebase_service_cert_url,
+  client_x509_cert_url: firebase_service_client_cert_url,
+  universe_domain: firebase_service_universe_domain,
 };
--- lib/firebase_admin.ts
+++ lib/firebase_admin.ts
@@ -1,18 +1,12 @@
 // lib/serverFirebase.ts
+import { firebase_database_url, serviceAccount } from "@/constants/firebase";
 import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccount from "@/keys/firebase_service.json";
-import { firebase_database_url } from "@/constants/firebase";
 
 let app: admin.app.App;
 
 if (!admin.apps.length) {
-  const finalServiceAccount =
-    serviceAccount ||
-    (JSON.parse(
-      process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}",
-    ) as ServiceAccount);
   app = admin.initializeApp({
-    credential: admin.credential.cert(finalServiceAccount as ServiceAccount),
+    credential: admin.credential.cert(serviceAccount as ServiceAccount),
     databaseURL: firebase_database_url,
   });
 } else {
